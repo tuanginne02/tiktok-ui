9acd63228d927eb59bde0d065f7c7ca4584a6f9d
TuQuocTuan <tuquoctuan0711@gmail.com>
2022-12-27 13:02:04 +0700

    Upadate 27/12

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,19 @@
+import classNames from 'classnames/bind';
+import style from './Header.module.scss';
+
+const cx = classNames.bind(style);
+
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('content')}>
+        <div className={cx('logo')}></div>
+        <div className={cx('search')}></div>
+        <div className={cx('upload')}></div>
+        <div className={cx('account')}></div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
